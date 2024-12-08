01983e612a2a9e6cb16ed048fff753d34213cfad
Kunal Gurav <kunalgurav961@gmail.com>
2024-12-08 16:07:20 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+
+function showAlert(event) {
+    event.preventDefault(); // Prevents the form from actually submitting
+    alert("Your form was sent successfully!\nYou can contact me on 9028908958");
+}
